d438b7b949ecac1462995ef6da5236a5042804fd
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-23 16:30:27 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,13 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  list = []
+  index = 0 
+  while index < source.length do
+    list[index] = source[index][:name]
+    index += 1 
+  end
+  list
 end
 
 def total_gross(source)
@@ -38,6 +45,14 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  index = 0 
+  total = 0
+  while index < source.size do
+    key = list_of_directors(source)[index]
+    total += directors_totals(source)[key]
+    index += 1 
+  end
+  total
 end
 
 
